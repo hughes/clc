c628a167520f04ac5d3ba05503f1dcc65cf7694f
Matt Hughes <hughesm02@gmail.com>
2014-03-07 21:43:00 +0000

    basic broken input

--- clc/main.py
+++ clc/main.py
@@ -2,31 +2,45 @@
 import curses
 import curses.textpad
 import time
+from clc import Clc
 
 class ClcUI():
     def __init__(self):
         self.screen = None
         self.input_scr = None
+        self.running = False
+        self.maxY = None
+        self.maxX = None
+
+        self.clc = Clc()
 
     def _start(self, stdscr):
         self.screen = stdscr
-        maxY, maxX = self.screen.getmaxyx()
-        self.input_scr = self.screen.subwin(maxY-1, 0)
-        self.hint_scr = self.screen.subwin(maxY-2, 0)
+        self.maxY, self.maxX = self.screen.getmaxyx()
+        self.input_scr = self.screen.subwin(self.maxY-1, 2)
+        self.hint_scr = self.screen.subwin(self.maxY-2, 0)
 
         self._loop()
 
     def _loop(self):
-        self.render()
-        time.sleep(1)
+        while self.running:
+            self.render()
 
     def start(self):
+        self.running = True
         curses.wrapper(self._start)
 
     def render(self):
-        self.input_scr.addstr('waiting for input')
+        self.screen.addstr(self.maxY-1, 0, '> ')
         self.hint_scr.addstr('this is a hint!')
+        self.input_pad = curses.textpad.Textbox(self.input_scr)
         self.screen.refresh()
+        try:
+            equation = self.input_pad.edit()
+        except KeyboardInterrupt:
+            self.running = False
+        else:
+            self.clc.push(equation)
 
 def main():
     ui = ClcUI()
